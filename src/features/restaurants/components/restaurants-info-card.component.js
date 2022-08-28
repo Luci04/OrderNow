1586e0c82ef399588a2664b55a98eb42ede6f134
luci04 <avinashukla2000@gmail.com>
2022-08-28 14:19:20 +0530

    Added Ratings and Open Svg to Card

--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -1,8 +1,10 @@
-/* eslint-disable prettier/prettier */
 import React from "react";
 import styled from "styled-components/native";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
   background-color: white;
@@ -14,8 +16,39 @@ const RestaurantCardCover = styled(Card.Cover)`
 `;
 
 const Title = styled.Text`
-  font-family:${(props) => props.theme.fonts.body}
+  font-family:${(props) => props.theme.fonts.heading};
+  font-size:${(props) => props.theme.fontSizes.body}
+  color:${(props) => props.theme.colors.ui.primary};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Info = styled(View)`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled(View)`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Open = styled(SvgXml)`
+  flex-direction: row;
 `;
 
 const RestaurantInfo = ({ restaurant = {} }) => {
@@ -28,15 +61,30 @@ const RestaurantInfo = ({ restaurant = {} }) => {
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgcfas8NKYAEx3f4nl-H4Onh6JnouK6YExMumJKOJi&s",
     ],
     address = "100 some random Street",
-    isOpenNoew = true,
+    isOpenNow = true,
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.ceil(rating)));
+
   return (
     <RestaurantCard mode="elevated" elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: `${photos[0]}` }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map((item, index) => (
+              <SvgXml key={index} xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isOpenNow && <Open xml={open} width={20} height={20} />}
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
