44554ab25d5f9ab196eb3d5a077498a1ce38bb93
luci04 <avinashukla2000@gmail.com>
2022-08-28 12:26:51 +0530

    Added Styled Components

--- src/features/restaurants/screens/restaurants-info-screen.js
+++ src/features/restaurants/screens/restaurants-info-screen.js
@@ -1,35 +1,37 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
+import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
-import { View, StyleSheet, SafeAreaView, Platform } from "react-native";
+import { SafeAreaView, Platform } from "react-native";
 import { StatusBar } from "react-native";
 import RestaurantInfoCard from "../components/restaurants-info-card.component";
 
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${Platform.OS === "android" ? StatusBar.currentHeight : 0}px;
+`;
+
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
+
 const RestaurantsScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
+    <SafeArea>
+      <SearchContainer>
         <Searchbar />
-      </View>
-      <View style={styles.list}>
+      </SearchContainer>
+      <RestaurantListContainer>
         <RestaurantInfoCard />
-      </View>
-    </SafeAreaView>
+      </RestaurantListContainer>
+    </SafeArea>
   );
 };
 
 export default RestaurantsScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-  },
-});
